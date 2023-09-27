3d9e908e6e9df958b229c5f8c805cc625fefabaa
Ethan Key <ethanrkey23@gmail.com>
2023-09-27 13:56:01 -0400

    code

--- tutorial/ViewController.swift
+++ tutorial/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Text to Image", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
 
-
+    @objc func didTapButton() {
+        getRandomPhoto()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(
+            x: 30,
+            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 55)
+    }
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+    
+    
 }
 
